81b690a77daff124e21a94f20d0b3d02618d5bfc
Vasp1 <43322006+Vasp1@users.noreply.github.com>
2020-09-16 12:26:41 +0100

    Follow Indicator

---
+++ src/modules/follow_indicator/index.js
@@ -0,0 +1,82 @@
+const settings = require('../../settings');
+const domObserver = require('../../observers/dom');
+const $ = require('jquery');
+const twitch = require('../../utils/twitch');
+const watcher = require('../../watcher');
+const twitchAPI = require('../../utils/twitch-api');
+const moment = require('moment');
+
+const USER_CARD_SELECTOR = 'div[data-a-target="about-panel"]';
+const FOLLOW_INDICATOR_ID = 'bttv-follow-indicator';
+
+const indicatorTemplate = `
+    <div class='tw-align-center tw-strong' id=${FOLLOW_INDICATOR_ID}>
+        <div>Follows you</div>
+        <div class="follow-tooltip"></div>
+    </div>
+`;
+
+let $followIndicator;
+let removeUserCardListener;
+
+class FollowIndicatorModule {
+    constructor() {
+        settings.add({
+            id: 'followIndicator',
+            name: 'Follow Indicator',
+            defaultValue: false,
+            description: 'Adds following you indicator inside user-card below video-player.'
+        });
+        settings.on('changed.followIndicator', value => value === true ? this.load() : this.unload());
+        watcher.on('load.channel', () => this.load());
+        watcher.on('load.chat', () => this.load());
+    }
+
+    load() {
+        if (settings.get('followIndicator') === false) return;
+
+        const currentChannel = twitch.getCurrentChannel();
+        const currentUser    = twitch.getCurrentUser();
+        if (!currentUser) return;
+
+        if (currentChannel.id === currentUser.id) return;
+
+        if (!$followIndicator) {
+            $followIndicator = $(indicatorTemplate);
+        }
+
+        removeUserCardListener = domObserver.on('.social-media-space', (node, isConnected) => {
+            if (!isConnected || node.getAttribute('data-a-target') !== 'about-panel') return;
+            this.embedIndicator();
+        });
+
+        this.updateFollowingIndicatorState(currentUser.id, currentChannel.id);
+    }
+
+    embedIndicator() {
+        if ($(`#${FOLLOW_INDICATOR_ID}`).length) return;
+        const $usercard = $(USER_CARD_SELECTOR).find('.social-media-space--content').find('.tw-pd-1');
+        const $followers = $usercard.find('.tw-align-center');
+        $followIndicator.insertBefore($followers);
+    }
+
+    updateFollowingIndicatorState(userId, channelId) {
+        return twitchAPI.get(`users/${channelId}/follows/channels/${userId}`)
+            .then(({created_at: createdAt}) => {
+                const since = moment(createdAt);
+                $('.follow-tooltip').text(`Since ${since.fromNow()}.`);
+                $followIndicator.show();
+            })
+            .catch(() => {
+                $followIndicator.hide();
+            });
+    }
+
+    unload() {
+        removeUserCardListener && removeUserCardListener();
+        $followIndicator && $followIndicator.remove();
+    }
+}
+
+module.exports = new FollowIndicatorModule();
+
